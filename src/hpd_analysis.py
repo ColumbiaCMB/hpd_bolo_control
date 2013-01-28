a4a833933c4ae698879240f443995f39336347c3
gjones <gej2113@columbia.edu>
2013-01-28 16:36:03 -0500

    added additional cryo plots

--- src/hpd_analysis.py
+++ src/hpd_analysis.py
@@ -10,6 +10,9 @@ def plotTemps(filename):
     fr = nc.groups['fridge']
     bt = fr.variables['bridge_temp_value'][:]
     magcur = fr.variables['dvm_volts_1'][:]
+    t60k = fr.variables['therm_temperature_0'][:]
+    tpillk = fr.variables['therm_temperature_1'][:]
+    t4k = fr.variables['therm_temperature_2'][:]
     f = plt.figure()
     ax = f.add_subplot(111)
     ax.plot(t/3600., bt, lw=2)
@@ -25,5 +28,18 @@ def plotTemps(filename):
     ax.set_ylabel('Temperature (K)')
     ax.set_title('Package temperature vs. magnet current\n%s' % filename)
     ax.grid()
+    
+    f = plt.figure()
+    ax = f.add_subplot(111)
+    ax.plot(t/3600,t4k,lw=2, label='4K Board Temp')
+    ax.plot(t/3600,tpillk,lw=2,label='ADR Pill Temp')
+    ax.plot(t/3600,t60k,lw=2,label='60 K stage Temp')
+    ax.set_xlabel('Time (hours)')
+    ax.set_ylabel('Temperature (K)')
+    ax.set_title('Cryostat temperatures vs. time\n%s' % filename)
+    ax.legend(loc='upper left')
+    ax.grid()    
+    
     nc.close()
     return t,bt,magcur
+    
